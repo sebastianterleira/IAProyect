3164cc612aecea0ea3fcf9ef1e2b960e85aa2145
sebastianterleira <seb.terleira1204@gmail.com>
2023-08-01 18:50:41 -0500

    First comming 🚀

---
+++ ia-proyect/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:react/jsx-runtime',
+		'standard',
+		'eslint-config-prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {
+		'react/prop-types': 'off',
+		'dot-notation': 'off',
+		"camelcase": 0,
+	},
+};
