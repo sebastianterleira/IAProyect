3474fe948017ef786a9e97a1fc941cacc25c6241
sebastianterleira <seb.terleira1204@gmail.com>
2023-08-02 17:38:36 -0500

    Add: component Navbar in router page

--- ia-proyect/app/page.js
+++ ia-proyect/app/page.js
@@ -1,9 +1,10 @@
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home/page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        
+        <Navbar/>
     </main>
   )
 }
