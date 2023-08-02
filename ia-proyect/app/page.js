5b67f42bb0351b5ba7a5809810eed67c012810df
Sebastian Bustamante <sbustamante301@gmail.com>
2023-08-02 15:11:48 -0500

    Add toggleLanguage button

--- ia-proyect/app/page.js
+++ ia-proyect/app/page.js
@@ -1,9 +1,10 @@
 import styles from '../styles/Home/page.module.css'
+import Button from '@/components/Button';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-        
+        <Button />
     </main>
   )
 }
