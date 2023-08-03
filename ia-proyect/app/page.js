6aa0f6b37f59b95c2a836f0dc074f95d773e3e75
sebastianterleira <seb.terleira1204@gmail.com>
2023-08-03 17:13:28 -0500

    Add: Component ExampleIA

--- ia-proyect/app/page.js
+++ ia-proyect/app/page.js
@@ -1,6 +1,7 @@
 import Pov from '@/components/Pov';
 import Navbar from '../components/Navbar';
 import styles from '../styles/Home/page.module.css';
+import ExampleIA from '@/components/subcomponents/ExampleIA';
 
 export default function Home() {
 	return (
@@ -8,6 +9,7 @@ export default function Home() {
 			<Navbar />
 			<main className={styles.main}>
 				<Pov />
+				<ExampleIA />
 			</main>
 		</>
 	);
