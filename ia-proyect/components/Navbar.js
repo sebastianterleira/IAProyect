7b64e99445ce5b478b60a4a55ba8973dcfcea606
sebastianterleira <seb.terleira1204@gmail.com>
2023-08-03 14:17:19 -0500

    Add: Component Button

--- ia-proyect/components/Navbar.js
+++ ia-proyect/components/Navbar.js
@@ -1,15 +1,20 @@
 import styles from '../styles/Navbar/Navbar.module.css';
 import Link from 'next/link';
 import { poppins } from '../styles/fonts';
+import Button from './Button';
 
 export default function Navbar() {
 	return (
 		<header className={styles.header}>
 			<div className={styles['header__container']}>
-				<Link href={"/"} className={`${styles['container__inicio']} ${poppins.className}`}>
+				<Link
+					href={'/'}
+					className={`${styles['container__inicio']} ${poppins.className}`}
+				>
 					Inicio
 				</Link>
-				<div>
+				<div className={styles['subContainer']}>
+					<Button />
 					<Link
 						href={'/chat'}
 						className={`${styles['container__goChat']} ${poppins.className} ${styles.unselected}`}
