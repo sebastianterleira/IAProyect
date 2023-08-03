5d04e2f29f3c4e17240b2c904287e7db12cceeec
sebastianterleira <seb.terleira1204@gmail.com>
2023-08-02 23:02:21 -0500

    Add: components

--- ia-proyect/app/page.js
+++ ia-proyect/app/page.js
@@ -1,12 +1,16 @@
-import Navbar from '../components/Navbar'
-import styles from '../styles/Home/page.module.css'
+import Pov from '@/components/Pov';
+import Navbar from '../components/Navbar';
+import styles from '../styles/Home/page.module.css';
 import Button from '@/components/Button';
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-        <Navbar/>
-        <Button />
-    </main>
-  )
+	return (
+		<>
+			<Navbar />
+			<main className={styles.main}>
+				<Button />
+				<Pov />
+			</main>
+		</>
+	);
 }
