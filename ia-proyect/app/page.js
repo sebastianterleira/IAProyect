48238a409319b3b14dc4c21a063fc3b4bf47b021
sebastianterleira <seb.terleira1204@gmail.com>
2023-08-03 14:17:06 -0500

    Mod: Delete Component Button

--- ia-proyect/app/page.js
+++ ia-proyect/app/page.js
@@ -1,14 +1,12 @@
 import Pov from '@/components/Pov';
 import Navbar from '../components/Navbar';
 import styles from '../styles/Home/page.module.css';
-import Button from '@/components/Button';
 
 export default function Home() {
 	return (
 		<>
 			<Navbar />
 			<main className={styles.main}>
-				<Button />
 				<Pov />
 			</main>
 		</>
