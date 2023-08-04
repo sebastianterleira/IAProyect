3c75a31fa91dcf50d53abbb0cfb95e1dd87d3efd
Sebastian Bustamante <sbustamante301@gmail.com>
2023-08-04 13:27:33 -0500

    Add flagtogglebutton

--- ia-proyect/app/page.js
+++ ia-proyect/app/page.js
@@ -3,7 +3,7 @@ import Pov from '@/components/Pov';
 import Navbar from '../components/Navbar';
 import styles from '../styles/Home/page.module.css';
 import Button from '@/components/Button';
-import Flag from 'react-flagkit';
+
 
 export default function Home() {
 	
--- ia-proyect/components/Flags.js
+++ ia-proyect/components/Flags.js
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import { FlagIcon } from 'react-flag-kit';
 
 const Flags = ({ country }) => {
